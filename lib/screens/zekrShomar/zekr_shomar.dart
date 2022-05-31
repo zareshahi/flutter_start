9c0ec6f4eb691f862d2a05bd04ae3a586ec69828
Ali Zareshahi <a.zareshahi1377@gmail.com>
2022-05-31 17:54:43 +0430

    feat: :zap: add exit on home page back button press

--- lib/screens/zekrShomar/zekr_shomar.dart
+++ lib/screens/zekrShomar/zekr_shomar.dart
@@ -99,6 +99,28 @@ class _ZekrShomarState extends State<ZekrShomar> {
     );
   }
 
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('هشدار'),
+            content: const Text('آیا میخواهید از برنامه خارج شوید؟'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('نه'),
+              ),
+              TextButton(
+                onPressed: () =>
+                    SystemChannels.platform.invokeMethod('SystemNavigator.pop'),
+                child: const Text('بله'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   void initState() {
     print('init state');
@@ -112,88 +134,91 @@ class _ZekrShomarState extends State<ZekrShomar> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: InkWell(
-                onTap: _counterCount,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    textDirection: TextDirection.rtl,
-                    children: [
-                      Text(
-                        widget.zekr,
-                        style: const TextStyle(fontSize: 32),
-                        textDirection: TextDirection.rtl,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(
-                        height: 64,
-                      ),
-                      Text(
-                        '${widget.zekrCounted}',
-                        style: TextStyle(fontSize: _fontSize ?? 50),
-                      ),
-                      SizedBox(
-                        height: Get.height / 6,
-                      ),
-                    ],
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: InkWell(
+                  onTap: _counterCount,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      textDirection: TextDirection.rtl,
+                      children: [
+                        Text(
+                          widget.zekr,
+                          style: const TextStyle(fontSize: 32),
+                          textDirection: TextDirection.rtl,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 64,
+                        ),
+                        Text(
+                          '${widget.zekrCounted}',
+                          style: TextStyle(fontSize: _fontSize ?? 50),
+                        ),
+                        SizedBox(
+                          height: Get.height / 6,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
+            ],
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     _showAlertDialog(context);
+        //   },
+        //   tooltip: 'بازنشانی',
+        //   child: const Icon(Icons.settings_backup_restore),
+
+        // ),
+        floatingActionButton: SpeedDial(
+          icon: Icons.menu,
+          activeIcon: Icons.close,
+          // backgroundColor: Colors.amber,
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.settings),
+              label: 'تنظیمات',
+              // backgroundColor: Colors.amberAccent,
+              onTap: () {
+                Get.to(() => const Settings());
+              },
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.list),
+              label: 'لیست اذکار',
+              // backgroundColor: Colors.amberAccent,
+              onTap: () {
+                Get.to(() => const ZekrList());
+                /* Do something */
+              },
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.settings_backup_restore),
+              label: 'صفر کردن شمارنده',
+              // backgroundColor: Colors.amberAccent,
+              onTap: () {
+                _showAlertDialog(context);
+              },
+            ),
+            SpeedDialChild(
+              child: const Icon(Icons.info),
+              label: 'درباره ما',
+              // backgroundColor: Colors.amberAccent,
+              onTap: () {/* Do something */},
             ),
           ],
         ),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     _showAlertDialog(context);
-      //   },
-      //   tooltip: 'بازنشانی',
-      //   child: const Icon(Icons.settings_backup_restore),
-
-      // ),
-      floatingActionButton: SpeedDial(
-        icon: Icons.menu,
-        activeIcon: Icons.close,
-        // backgroundColor: Colors.amber,
-        children: [
-          SpeedDialChild(
-            child: const Icon(Icons.settings),
-            label: 'تنظیمات',
-            // backgroundColor: Colors.amberAccent,
-            onTap: () {
-              Get.to(() => const Settings());
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.list),
-            label: 'لیست اذکار',
-            // backgroundColor: Colors.amberAccent,
-            onTap: () {
-              Get.to(() => const ZekrList());
-              /* Do something */
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.settings_backup_restore),
-            label: 'صفر کردن شمارنده',
-            // backgroundColor: Colors.amberAccent,
-            onTap: () {
-              _showAlertDialog(context);
-            },
-          ),
-          SpeedDialChild(
-            child: const Icon(Icons.info),
-            label: 'درباره ما',
-            // backgroundColor: Colors.amberAccent,
-            onTap: () {/* Do something */},
-          ),
-        ],
       ),
     );
   }
